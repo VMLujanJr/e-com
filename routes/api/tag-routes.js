92fdcc2d0333e552ee2ab6c1ae8b955f388812f0
VMLujanJr <vmlujanjr@outlook.com>
2022-05-31 00:46:07 -0500

    updated all get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,14 +2,51 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 /* ENDPOINT: /api/tags */
 
+// Get All... /api/tags
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => { 
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+// Get One... /api/tags/:id
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'There is no tag associated with this ID.'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
